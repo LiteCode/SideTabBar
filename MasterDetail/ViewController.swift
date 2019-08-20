4a2845976bf4b36e866677f2f3364b9bb5a4ec84
Vladislav Prusakov <spectraldragonchannel@gmail.com>
2019-08-20 16:18:50 +0300

    Initial Commit

---
+++ MasterDetail/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MasterDetail
+//
+//  Created by Vladislav Prusakov on 20.08.2019.
+//  Copyright © 2019 Vladislav Prusakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
