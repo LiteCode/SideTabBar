f3f19f0d78ed89190f7822fc27ae3cf3697119d1
Vladislav Prusakov <v.a.prusakov@tinkoff.ru>
2019-08-28 19:18:59 +0300

    Fix tab bar

--- MasterDetail/ViewController.swift
+++ MasterDetail/ViewController.swift
@@ -21,13 +21,6 @@ class TabBarButton: UIControl {
         get { return self.stackView.axis }
         set { self.stackView.axis = newValue }
     }
-//
-//    private let titleLabel: UILabel = {
-//        let label = UILabel()
-//        label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
-//        label.minimumScaleFactor = 0.8
-//        return label
-//    }()
 
     override var isSelected: Bool {
         didSet {
@@ -54,9 +47,6 @@ class TabBarButton: UIControl {
 
         self.imageView.image = item.image?.withRenderingMode(.alwaysTemplate).withAlignmentRectInsets(item.imageInsets)
 
-//        self.titleLabel.text = item.title
-//        self.titleLabel.isHidden = item.title == nil
-//        self.titleLabel.textAlignment = .center
         self.tabBarItem = item
     }
 
@@ -70,22 +60,15 @@ class TabBarButton: UIControl {
 
         imageContainerView.addSubview(imageView)
 
-//        let imageViewLeftAnchor = imageView.leftAnchor.constraint(equalTo: imageContainerView.leftAnchor, constant: 8)
-//        imageViewLeftAnchor.priority = UILayoutPriority(rawValue: 800)
-
         imageContainerView.isUserInteractionEnabled = false
         imageView.isUserInteractionEnabled = false
         stackView.isUserInteractionEnabled = false
-//        titleLabel.isUserInteractionEnabled = false
-//        self.backgroundColor = .blue
 
         NSLayoutConstraint.activate([
             imageView.centerXAnchor.constraint(equalTo: imageContainerView.centerXAnchor),
             imageView.centerYAnchor.constraint(equalTo: imageContainerView.centerYAnchor),
-//            imageView.bottomAnchor.constraint(equalTo: imageContainerView.bottomAnchor, constant: 10),
             imageView.widthAnchor.constraint(equalToConstant: 28),
             imageView.heightAnchor.constraint(equalToConstant: 28),
-//            imageViewLeftAnchor
         ])
 
         self.addSubview(stackView)
@@ -98,17 +81,13 @@ class TabBarButton: UIControl {
             stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
             imageContainerView.heightAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: 1)
         ])
-
-//        stackView.addArrangedSubview(titleLabel)
     }
 
     private func updateTintColor() {
         if isSelected {
             self.imageView.tintColor = itemTintColor
-//            self.titleLabel.textColor = itemTintColor
         } else {
             self.imageView.tintColor = itemUnselectedTintColor
-//            self.titleLabel.textColor = itemUnselectedTintColor
         }
     }
 
@@ -138,6 +117,10 @@ class TabBar: UITabBar {
     override var selectedItem: UITabBarItem? {
         willSet {
             self.selectedHandler?(newValue)
+            
+            if newValue == nil {
+                self.selectedButton = nil
+            }
         }
     }
     
@@ -165,8 +148,8 @@ class TabBar: UITabBar {
         self.addSubview(stackView)
         
         NSLayoutConstraint.activate([
-            stackView.topAnchor.constraint(equalTo: self.topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            stackView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: self.safeAreaLayoutGuide.bottomAnchor),
             stackView.leftAnchor.constraint(equalTo: self.leftAnchor),
             stackView.rightAnchor.constraint(equalTo: self.rightAnchor)
         ])
@@ -188,15 +171,17 @@ class TabBar: UITabBar {
 
             stackView.addArrangedSubview(UIView())
         }
-        
-        self.selectedItem = items?.first
-        self.selectedButton = buttons.first
         _items = items
     }
     
     @objc private func onTabButtonPressed(_ button: TabBarButton) {
-        self.selectedItem = button.tabBarItem
-        self.selectedButton = button
+        if selectedButton === button {
+            self.selectedItem = nil
+            self.selectedButton = nil
+        } else {
+            self.selectedItem = button.tabBarItem
+            self.selectedButton = button
+        }
     }
     
     func onTabSelectedHandler(_ block: @escaping (UITabBarItem?) -> Void) {
@@ -223,8 +208,9 @@ class TabBarViewController: UIViewController {
     
     private weak var tabBar: UITabBar?
     
-    private weak var detailContainerView: UIView!
+    private weak var detailContainerView: UIVisualEffectView!
     private weak var mainContainerView: UIView!
+    private weak var overlayView: UIView!
     
     private weak var detailWidthConstraint: NSLayoutConstraint?
     
@@ -246,9 +232,16 @@ class TabBarViewController: UIViewController {
         let detailContainerView = UIVisualEffectView()
         detailContainerView.effect = UIBlurEffect(style: .systemChromeMaterial)
         detailContainerView.translatesAutoresizingMaskIntoConstraints = false
-        detailContainerView.backgroundColor = .brown
         view.addSubview(detailContainerView)
         self.detailContainerView = detailContainerView
+        
+        let overlayView = UIView()
+        overlayView.alpha = 0
+        overlayView.backgroundColor = UIColor.black.withAlphaComponent(0.3)
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        overlayView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onOverlayPressed(_:))))
+        view.addSubview(overlayView)
+        self.overlayView = overlayView
         
         let tabBar = TabBar()
         tabBar.onTabSelectedHandler { item in
@@ -259,12 +252,12 @@ class TabBarViewController: UIViewController {
         view.addSubview(tabBar)
         self.tabBar = tabBar
         
-        let detailWidthConstraint = detailContainerView.widthAnchor.constraint(equalToConstant: Constants.detailPresentedWidth)
+        let detailWidthConstraint = detailContainerView.widthAnchor.constraint(equalToConstant: Constants.detailHiddenWidth)
         
         NSLayoutConstraint.activate([
             tabBar.topAnchor.constraint(equalTo: view.topAnchor),
             tabBar.leftAnchor.constraint(equalTo: view.leftAnchor),
-            tabBar.bottomAnchor.constraint(greaterThanOrEqualTo: view.bottomAnchor),
+            tabBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             tabBar.widthAnchor.constraint(equalToConstant: Constants.width),
             
             mainContainerView.leftAnchor.constraint(equalTo: view.leftAnchor),
@@ -276,6 +269,11 @@ class TabBarViewController: UIViewController {
             detailContainerView.leftAnchor.constraint(equalTo: tabBar.rightAnchor),
             detailContainerView.topAnchor.constraint(equalTo: view.topAnchor),
             detailContainerView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            overlayView.leftAnchor.constraint(equalTo: detailContainerView.rightAnchor),
+            overlayView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            overlayView.topAnchor.constraint(equalTo: view.topAnchor),
+            overlayView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
         
         self.additionalSafeAreaInsets.left = Constants.width
@@ -284,35 +282,58 @@ class TabBarViewController: UIViewController {
         
         // MARK: - Mock
         
-        let firstController = TestViewController(tabBarItem: UITabBarItem(title: nil, image: #imageLiteral(resourceName: "documents"), tag: 0))
+        let firstController = TableTestViewController(tabBarItem: UITabBarItem(title: "Some new message", image: #imageLiteral(resourceName: "documents"), tag: 0))
         
         let secondController = TestViewController(tabBarItem: UITabBarItem(title: "TestAgain", image: #imageLiteral(resourceName: "finance"), tag: 1))
         
         self.setViewControllers([firstController, secondController], animated: false)
+        
+        let mainController = TableTestViewController(tabBarItem: UITabBarItem(title: "Some new message", image: #imageLiteral(resourceName: "documents"), tag: 0))
+        
+        self.showMasterViewController(mainController, sender: nil)
     }
     
     private var isDetailPresented: Bool {
         return self.detailWidthConstraint?.constant == Constants.detailPresentedWidth
     }
     
+    private weak var selectedViewController: UIViewController?
+    
     private func setVisibleDetailViewController(for tabBarItem: UITabBarItem?) {
         
-        let animator = UIViewPropertyAnimator(duration: 0.3, timingParameters: UICubicTimingParameters(animationCurve: .easeIn))
+        let animator = UIViewPropertyAnimator(duration: 0.15, timingParameters: UICubicTimingParameters(animationCurve: .easeIn))
         
         if let item = tabBarItem {
             
             guard let viewControllerToShow = self.viewControllers.first(where: { $0.tabBarItem === item }) else { return }
             
-            self.addChildViewController(viewControllerToShow, viewContainer: self.detailContainerView)
+            if viewControllerToShow === self.selectedViewController { return }
             
+            animator.addAnimations {
+                self.overlayView.alpha = 1
+            }
+            
+            self.selectedViewController?.removeFromParentViewController()
+            self.addChildViewController(viewControllerToShow, viewContainer: self.detailContainerView.contentView)
+            self.selectedViewController = viewControllerToShow
             
             self.view.layoutIfNeeded()
             self.detailWidthConstraint?.constant = Constants.detailPresentedWidth
             
-
         } else {
             self.view.layoutIfNeeded()
             self.detailWidthConstraint?.constant = Constants.detailHiddenWidth
+            
+            animator.addAnimations {
+                self.overlayView.alpha = 0
+            }
+            
+            animator.addCompletion { state in
+                if state == .end {
+                    self.selectedViewController?.removeFromParentViewController()
+                    self.selectedViewController = nil
+                }
+            }
         }
         
         animator.addAnimations {
@@ -330,6 +351,15 @@ class TabBarViewController: UIViewController {
         self.tabBar?.setItems(tabBarItems, animated: animated)
     }
     
+    func showMasterViewController(_ vc: UIViewController, sender: Any?) {
+        self.addChildViewController(vc, viewContainer: self.mainContainerView)
+    }
+    
+    @objc private func onOverlayPressed(_ gesture: UITapGestureRecognizer) {
+        self.tabBar?.selectedItem = nil
+    }
+
+    
 }
 
 extension UIViewController {
@@ -343,12 +373,8 @@ extension UIViewController {
     func addChildViewController(_ viewController: UIViewController, viewContainer: UIView) {
         self.addChildViewController(viewController) { (view) in
             viewContainer.insertSubview(view, at: 0)
-            NSLayoutConstraint.activate([
-                view.leftAnchor.constraint(equalTo: viewContainer.leftAnchor),
-                view.rightAnchor.constraint(equalTo: viewContainer.rightAnchor),
-                view.topAnchor.constraint(equalTo: viewContainer.topAnchor),
-                view.bottomAnchor.constraint(equalTo: viewContainer.bottomAnchor)
-            ])
+            view.frame.size = viewContainer.frame.size
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         }
     }
     
@@ -362,9 +388,60 @@ extension UIViewController {
 }
 
 private class TestViewController: UIViewController {
+    
+    private weak var label: UILabel!
+    
     init(tabBarItem: UITabBarItem) {
         super.init(nibName: nil, bundle: nil)
         self.tabBarItem = tabBarItem
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let label = UILabel()
+        label.textColor = .black
+        label.text = self.tabBarItem.title
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        self.view.addSubview(label)
+        self.label = label
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            label.leftAnchor.constraint(equalTo: self.view.leftAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private class TableTestViewController: UITableViewController {
+    
+    init(tabBarItem: UITabBarItem) {
+        super.init(nibName: nil, bundle: nil)
+        self.tabBarItem = tabBarItem
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "identifier") ?? UITableViewCell(style: .default, reuseIdentifier: "identifier")
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "Some index \(indexPath.row)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
